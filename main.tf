f74d5ac4cfced5901e6380e92990b3249a1a01c1
Rajesh Kumar <rrp.b89@gmail.com>
2025-07-26 23:21:05 +0530

    code added

--- main.tf
+++ main.tf
@@ -19,3 +19,10 @@ resource "azurerm_resource_group" "priya2" {
   name     = "priyarg2"
   location = "West US"
 }
+resource "azurerm_storage_account" "priya" {
+  name                     = "priyastorageacct"
+  resource_group_name      = azurerm_resource_group.priya.name
+  location                 = azurerm_resource_group.priya.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
